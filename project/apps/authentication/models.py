ab8a302eab75e0071838c541a20e1dd3b447abe1
Do Van Thanh <huoquandace@gmail.com>
2022-12-19 20:20:17 +0700

    Update models.py

--- project/apps/authentication/models.py
+++ project/apps/authentication/models.py
@@ -45,19 +45,20 @@ class Profile(BaseModel):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    gender = models.CharField(_("gender"), max_length=100, blank=True, choices=Gender.choices)
+    degree = models.CharField(_("degree"), max_length=100, blank=True, null=True, choices=DEGREE_CHOICES)
+    marital_status = models.CharField(_("marital_status"), max_length=100, blank=True, null=True, choices=MARITAL_STATUS_CHOICES)
+
     first_name = models.CharField(_("first name"), max_length=100, blank=True)
     last_name = models.CharField(_("last name"), max_length=100, blank=True)    # Surname
     phone = models.CharField(_("phone"), max_length=100, blank=True, null=True)
     birthday = models.DateField(_("birthday"), max_length=10, blank=True, null=True)
-    gender = models.CharField(_("gender"), max_length=100, blank=True, choices=Gender.choices)
     age = models.IntegerField(_("age"), blank=True, null=True)
     address = models.CharField(_("address"), max_length=100, blank=True, null=True)
     avatar = models.ImageField(default='images/avatar_default1.jpg', upload_to='images')
     citizen_identification = models.CharField(_("citizen identification"), max_length=100, blank=True, null=True)
     tax_code = models.CharField(_("tax code"), max_length=100, blank=True, null=True)
-    degree = models.CharField(_("degree"), max_length=100, blank=True, null=True, choices=DEGREE_CHOICES)
     certificate = models.TextField(_("certificate"), blank=True, null=True)
-    marital_status = models.CharField(_("marital_status"), max_length=100, blank=True, null=True, choices=MARITAL_STATUS_CHOICES)
     license_plates = models.CharField(_("license_plates"), max_length=100, blank=True, null=True,)
 
     def __str__(self):
