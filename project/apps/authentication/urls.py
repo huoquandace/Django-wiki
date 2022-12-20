5951b21002cba81df693461c9b470adcefabe097
Do Van Thanh <120877704+huoquandace@users.noreply.github.com>
2022-12-20 12:41:20 +0700

    Update urls.py

--- project/apps/authentication/urls.py
+++ project/apps/authentication/urls.py
@@ -6,4 +6,5 @@ from authentication.views import *
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('profile/', Profile.as_view(), name='profile'),
+    path('register/', Register.as_view(), name='register'),
 ]
