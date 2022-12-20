1fe598d21c2d5fb69dddb3622d448e6820a7f852
Do Van Thanh <120877704+huoquandace@users.noreply.github.com>
2022-12-20 19:42:38 +0700

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -12,4 +12,5 @@ urlpatterns += [
     path('auth/', include('authentication.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
