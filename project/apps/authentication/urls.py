474c1674d0060e595378a8ce0c62077e8ac35222
Do Van Thanh <120877704+huoquandace@users.noreply.github.com>
2022-12-20 15:32:36 +0700

    up

--- project/apps/authentication/urls.py
+++ project/apps/authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('profile/', Profile.as_view(), name='profile'),
     path('register/', Register.as_view(), name='register'),
+    path('register/done/', RegisterDone.as_view(), name='register_done'),
 ]
--- project/apps/authentication/views.py
+++ project/apps/authentication/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from pprint import pprint
+
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, FormView
@@ -15,9 +19,23 @@ class Register(FormView):
 
     def form_valid(self, form):
         data = form.cleaned_data
-        from pprint import pprint
-        pprint(data)
-        pass
+        pprint(data) # DEBUG
+        new_user = get_user_model().objects.create_user(
+            username = data['username'],
+            password = data['password1'],
+            email = data['email'],
+        )
+        url = f"{reverse('register_done')}?username={new_user.username}"
+        pprint(url) # DEBUG
+        return redirect(url)
+
+class RegisterDone(TemplateView):
+    template_name = 'register_done.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['username'] = self.request.GET.get('username')
+        return context
 
 class Profile(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
