17df153687d4ff39916cda356700b811ced5c327
Do Van Thanh <120877704+huoquandace@users.noreply.github.com>
2022-12-20 12:37:56 +0700

    up

--- project/apps/authentication/views.py
+++ project/apps/authentication/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 
 class Login(LoginView):
     template_name = 'login.html'
 
-class Register():
-    pass
+class Register(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
 
 class Profile(LoginRequiredMixin, TemplateView):
     template_name = 'profile.html'
