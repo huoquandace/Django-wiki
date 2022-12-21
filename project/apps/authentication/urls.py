d465639d40bf8cde6ce6380a9c3d96f033b78302
Portgas D.Ace <huoquandace@gmail.com>
2022-12-21 13:19:30 +0700

    up

--- project/apps/authentication/urls.py
+++ project/apps/authentication/urls.py
@@ -11,9 +11,8 @@ urlpatterns = [
     path('password_reset/', PasswordReset.as_view(), name='password_reset'),
     path('password_reset/done/', PasswordResetDone.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirm.as_view(), name='password_reset_confirm'),
-
-    path('reset/done/', Logout.as_view(), name='password_reset_complete'),
-
+    path('reset/done/', PasswordResetComplete.as_view(), name='password_reset_complete'),
+    
     path('profile/', Profile.as_view(), name='profile'),
     path('register/', Register.as_view(), name='register'),
     path('register/done/', RegisterDone.as_view(), name='register_done'),
