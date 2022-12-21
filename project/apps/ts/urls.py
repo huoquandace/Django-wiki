16b21fc7da6f5a7e23ae5ed034417e89bfd69d9f
Portgas D.Ace <huoquandace@gmail.com>
2022-12-21 16:35:45 +0700

    up

--- project/apps/ts/urls.py
+++ project/apps/ts/urls.py
@@ -5,4 +5,5 @@ from ts.views import *
 
 urlpatterns = [
     path('', Ts.as_view() ,name=''),
+    path('ts1/', Ts1.as_view() ,name='ts1'),
 ]
--- project/apps/ts/views.py
+++ project/apps/ts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from common.mixins import GroupRequiredMixin
 
 
 class Ts(TemplateView):
+    template_name = 'index.html'
+
+class Ts1(GroupRequiredMixin, TemplateView):
+    group_required = ['ts1']
     template_name = 'index.html'
