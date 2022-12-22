9edc6fdfb536d3b20d5c6ab6c9f1b7d78145b62d
Portgas D.Ace <huoquandace@gmail.com>
2022-12-22 13:32:57 +0700

    up

--- project/apps/authentication/routes/user_management_v1.py
+++ project/apps/authentication/routes/user_management_v1.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 from authentication.views.user_management_v1 import (
     Index,
+    userdata
 )
 
 urlpatterns = [
     path('', Index.as_view(), name=''),
+    # path('user_add_csv/', userdata, name='user_add_csv'),
 ]
--- project/apps/authentication/views/user_management_v1.py
+++ project/apps/authentication/views/user_management_v1.py
@@ -1,5 +1,30 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from csv import reader
+def userdata(request):
+    try:
+        with open('templates/csv.csv', 'r') as csv_file:
+            csvf = reader(csv_file)
+            data = []
+            for id, username, password, *__ in csvf:
+                user = User(username=username)
+                user.set_password(password)
+                data.append(user)
+            User.objects.bulk_create(data)
+        csv_file.close()
+    except Exception as e:
+        print(e)
+    return JsonResponse('user csv is now working', safe=False)
+
+
+class ImportUser(LoginRequiredMixin, View):
+    pass
