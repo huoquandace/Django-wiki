f740362f83e71f1deb87224c59ad51f95544faa4
Portgas D.Ace <huoquandace@gmail.com>
2022-12-22 16:11:24 +0700

    up

--- project/apps/core/views.py
+++ project/apps/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    # def get(self, request, *args, **kwargs):
+    #     messages.info(request, 'success')
+    #     return render(request, 'index.html')
