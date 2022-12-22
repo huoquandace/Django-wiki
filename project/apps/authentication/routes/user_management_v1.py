7914d7c5226c86aa7c86b4c0ba2e735eb766251a
Portgas D.Ace <huoquandace@gmail.com>
2022-12-22 14:53:41 +0700

    up

--- project/apps/authentication/routes/user_management_v1.py
+++ project/apps/authentication/routes/user_management_v1.py
@@ -4,9 +4,11 @@ from django.urls import path
 from authentication.views.user_management_v1 import (
     Index,
     ImportUser,
+    download
 )
 
 urlpatterns = [
     path('', Index.as_view(), name=''),
     path('user_add_csv/', ImportUser.as_view(), name='user_add_csv'),
+    path('download/', download, name='download'),
 ]
--- project/apps/authentication/views/user_management_v1.py
+++ project/apps/authentication/views/user_management_v1.py
@@ -1,16 +1,20 @@
+import os
 from csv import reader
 
+from django.conf import settings
 from django.shortcuts import redirect
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth import get_user_model
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, Http404
 
 from common.forms import UploadFileForm
 
 
-CSV_FILE_PATH = 'data/csv'
+CSV_FILE_PATH = 'data/csv/'
+USER_CSV_FILE_TEMPLALTE = 'data/csv.csv'
+
 
 class Index(TemplateView):
     template_name = 'index.html'
@@ -25,7 +29,7 @@ class ImportUser(LoginRequiredMixin, FormView):
         fs = FileSystemStorage(location=CSV_FILE_PATH)
         filename = fs.save(file.name, file)
         # uploaded_file_url = fs.url(filename) # If not set location in fs, Default to MEDIA_ROOT
-        file_path = CSV_FILE_PATH + '/' + filename
+        file_path = CSV_FILE_PATH + filename
         try:
             with open(file_path, 'r') as csv_file:
                 csvf = reader(csv_file)
@@ -40,3 +44,15 @@ class ImportUser(LoginRequiredMixin, FormView):
         except Exception as e:
             print(e)
         return JsonResponse('Successful', safe=False)
+
+
+
+def download(request):
+    # file_path = os.path.join(settings.MEDIA_ROOT, path)
+    file_path = os.path.join(settings.BASE_DIR, USER_CSV_FILE_TEMPLALTE)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
