73ba6aa54d77248a1b7dd685a99620353cc6aa64
Portgas D.Ace <huoquandace@gmail.com>
2022-12-22 14:40:24 +0700

    up

--- project/apps/authentication/routes/user_management_v1.py
+++ project/apps/authentication/routes/user_management_v1.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 from authentication.views.user_management_v1 import (
     Index,
-    userdata
+    ImportUser,
 )
 
 urlpatterns = [
     path('', Index.as_view(), name=''),
-    # path('user_add_csv/', userdata, name='user_add_csv'),
+    path('user_add_csv/', ImportUser.as_view(), name='user_add_csv'),
 ]
--- project/apps/authentication/views/user_management_v1.py
+++ project/apps/authentication/views/user_management_v1.py
@@ -1,30 +1,42 @@
-from django.views.generic import TemplateView, View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from csv import reader
 
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files.storage import FileSystemStorage
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+
+from common.forms import UploadFileForm
+
+
+CSV_FILE_PATH = 'data/csv'
 
 class Index(TemplateView):
     template_name = 'index.html'
 
 
-from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-User = get_user_model()
-from csv import reader
-def userdata(request):
-    try:
-        with open('templates/csv.csv', 'r') as csv_file:
-            csvf = reader(csv_file)
-            data = []
-            for id, username, password, *__ in csvf:
-                user = User(username=username)
-                user.set_password(password)
-                data.append(user)
-            User.objects.bulk_create(data)
-        csv_file.close()
-    except Exception as e:
-        print(e)
-    return JsonResponse('user csv is now working', safe=False)
+class ImportUser(LoginRequiredMixin, FormView):
+    form_class = UploadFileForm
+    template_name = 'user_management_v1/import.html'
 
-
-class ImportUser(LoginRequiredMixin, View):
-    pass
+    def form_valid(self, form):
+        file = form.cleaned_data['file']
+        fs = FileSystemStorage(location=CSV_FILE_PATH)
+        filename = fs.save(file.name, file)
+        # uploaded_file_url = fs.url(filename) # If not set location in fs, Default to MEDIA_ROOT
+        file_path = CSV_FILE_PATH + '/' + filename
+        try:
+            with open(file_path, 'r') as csv_file:
+                csvf = reader(csv_file)
+                User = get_user_model()
+                data = []
+                for id, username, password, *__ in csvf:
+                    user = User(username=username)
+                    user.set_password(password)
+                    data.append(user)
+                User.objects.bulk_create(data)
+            csv_file.close()
+        except Exception as e:
+            print(e)
+        return JsonResponse('user csv is now working', safe=False)
