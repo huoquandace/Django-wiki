d7255be413d363cbe298c780745570e87abbb2f4
Portgas D.Ace <huoquandace@gmail.com>
2023-01-10 13:27:41 +0700

    up

--- project/apps/authentication/routes/v1.py
+++ project/apps/authentication/routes/v1.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('profile/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile_update'),
     path('user_list/', UserList.as_view(), name='user_list'),
     path('user_list/to_pdf/', UserListToPdf.as_view(), name='user_list_to_pdf'), 
-    path('user_add_csv/', UserAddCsv.as_view(), name='user_add_csv'),
-    path('user_add_csv/download_template/', DowloadUserCsvTemplate.as_view(), name='download'),
+    path('user_add/', UserAddCsv.as_view(), name='user_add_csv'),
+    path('user_add/csv/', UserAddCsv.as_view(), name='user_add_csv'),
+    path('user_add/csv/download_template/', DowloadUserCsvTemplate.as_view(), name='download'),
 ]
