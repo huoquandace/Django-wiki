12e282a26fbcdc6c766f2521d1315b64791d78bf
Portgas D.Ace <huoquandace@gmail.com>
2023-01-13 13:17:32 +0700

    up

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
 
 urlpatterns += i18n_patterns (
