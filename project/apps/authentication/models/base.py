72a1279951db56f8f6cbd5db1845b11890fa4886
Portgas D.Ace <huoquandace@gmail.com>
2023-01-17 12:31:37 +0700

    up

--- project/apps/authentication/models/base.py
+++ project/apps/authentication/models/base.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
-from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib import auth
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_("username"), max_length=100, unique=True, validators=[UnicodeUsernameValidator()], error_messages={"unique": _("A user with that username already exists."),})
+class User(auth.models.AbstractBaseUser, auth.models.PermissionsMixin):
+    username = models.CharField(_("username"), max_length=100, unique=True, validators=[auth.validators.UnicodeUsernameValidator()], error_messages={"unique": _("A user with that username already exists."),})
     first_name = models.CharField(_("first name"), max_length=100, blank=True)
     last_name = models.CharField(_("last name"), max_length=100, blank=True)    # Surname
     email = models.EmailField(_("email address"), blank=True)
@@ -14,7 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_("active"), default=True)
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
-    objects = UserManager()
+    objects = auth.models.UserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
