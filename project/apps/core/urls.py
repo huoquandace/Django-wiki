4a83f61032d130e848f63b28e6a56f98f1330d58
Portgas D.Ace <huoquandace@gmail.com>
2023-02-01 13:02:47 +0700

    up

--- project/apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from core.views import *
-
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-]
--- project/apps/ts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from ts.views import *
-
-
-urlpatterns = [
-    path('', Ts.as_view() ,name=''),
-    path('ts1/', Ts1.as_view() ,name='ts1'),
-]
