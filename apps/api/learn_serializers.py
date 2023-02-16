2735880aa53bd0f091391463edc252e1c3aad888
Portgas D.Ace <huoquandace@gmail.com>
2023-02-16 09:31:53 +0700

    up

--- apps/api/learn_serializers.py
+++ apps/api/learn_serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from rest_framework.validators import ValidationError
 
 
