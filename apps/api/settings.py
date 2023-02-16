9c8b863b54fd4b7ecf3721aab5ea597ca5e10cae
Portgas D.Ace <huoquandace@gmail.com>
2023-02-16 10:30:23 +0700

    up

--- apps/api/settings.py
+++ apps/api/settings.py
@@ -1,4 +1,6 @@
-from settings.base import INSTALLED_APPS
+
+from datetime import timedelta
+from settings.base import INSTALLED_APPS, SECRET_KEY
 
 
 INSTALLED_APPS += [
@@ -6,3 +8,23 @@ INSTALLED_APPS += [
     'api',
     'drf_yasg',
 ]
+
+
+REST_FRAMEWORK = {
+    "NON_FIELD_ERRORS_KEY": "errors",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated"),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
