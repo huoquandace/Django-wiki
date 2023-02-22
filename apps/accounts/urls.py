3b6a7e192f5ce814f3f6b777a6a4f358619c51f8
Portgas D.Ace <huoquandace@gmail.com>
2023-02-22 14:45:13 +0700

    up

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,7 +5,7 @@ from accounts.views.base import *
 
 urlpatterns = [
     path('', AuthIndex.as_view() ,name='auth_index'),
-    path('auth/', include('accounts.routes.auth')),
-    path('account/', include('accounts.routes.account')),
-    path('user_manager/', include('accounts.routes.user_manager')),
+    path('auth/', include('accounts.routes.auth_route')),
+    path('account/', include('accounts.routes.account_route')),
+    path('user_manager/', include('accounts.routes.user_manager_route')),
 ]
